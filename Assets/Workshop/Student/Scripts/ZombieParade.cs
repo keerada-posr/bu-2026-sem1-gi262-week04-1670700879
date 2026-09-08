d1259c4f84796fa42910f0d58513d34098963b9d
keerada-posr <keerada.posr@bumail.net>
2026-09-08 17:10:53 +0700

    Workshop

--- Assets/Workshop/Student/Scripts/ZombieParade.cs
+++ Assets/Workshop/Student/Scripts/ZombieParade.cs
@@ -56,28 +56,57 @@ namespace Solution
 
             while (isAlive)
             {
-                // 1. ดึงส่วนแรกของงูออกมา
+                // 0. สร้างหัว
+                Parade.AddFirst(this.gameObject);
 
-                // 2. ดึงส่วนสุดท้ายของงูออกมา
-             
-                // 3. ลบส่วนสุดท้ายออกจาก LinkedList
+                while (isAlive)
+                {
+                    // 1. ดึงส่วนแรกของงูออกมา
+                    LinkedListNode<GameObject> firstNode = Parade.First;
+                    GameObject firstPart = firstNode.Value;
 
-                // 5. กำหนดตำแหน่งและทิศทางของส่วนที่ถูกย้ายมาใหม่
-                // ให้ไปอยู่ที่ตำแหน่งของส่วนหัวงู (ซึ่งเพิ่งเคลื่อนที่ไปเมื่อครู่)
-   
-                //6. เคลื่อนที่
+                    // 2. ดึงส่วนสุดท้ายของงูออกมา
+                    LinkedListNode<GameObject> lastNode = Parade.Last;
+                    GameObject lastPart = lastNode.Value;
 
-                // 7. เพิ่มส่วนนั้นกลับเข้าไปเป็นส่วนที่สองของ LinkedList
-                // (ซึ่งก็คือส่วนแรกของลำตัว)
+                    // 3. ลบส่วนสุดท้ายออกจาก LinkedList
+                    Parade.RemoveLast();
 
-                // รอตามเวลาที่กำหนดก่อนการเคลื่อนที่ครั้งต่อไป
-                yield return new WaitForSeconds(moveInterval);
+                    // 5. กำหนดตำแหน่งและทิศทางของส่วนที่ถูกย้ายมาใหม่
+                    // ให้ไปอยู่ที่ตำแหน่งของส่วนหัวงู (ซึ่งเพิ่งเคลื่อนที่ไปเมื่อครู่)
+                    int toX = 0;
+                    int toY = 0;
+                    bool isCollide = true;
+                    while (isCollide == true)
+                    {
+                        moveDirection = RandomizeDirection();
+                        toX = (int)(firstPart.transform.position.x + moveDirection.x);
+                        toY = (int)(firstPart.transform.position.y + moveDirection.y);
+                        isCollide = IsCollision(toX, toY);
+                    }
+
+                    //6. เคลื่อนที่
+                    mapGenerator.mapdata[positionX, positionY] = null;
+                    positionX = toX;
+                    positionY = toY;
+
+                    lastPart.transform.position = new Vector3(positionX, positionY, 0);
+                    mapGenerator.mapdata[positionX, positionY] = this;
+                    // 7. เพิ่มส่วนนั้นกลับเข้าไปเป็นส่วนที่สองของ LinkedList
+                    // (ซึ่งก็คือส่วนแรกของลำตัว)
+                    Parade.AddFirst(lastNode);
+                    // รอตามเวลาที่กำหนดก่อนการเคลื่อนที่ครั้งต่อไป
+                    yield return new WaitForSeconds(moveInterval);
+                }
             }
         }
         private bool IsCollision(int x, int y)
         {
             // 4. ตรวจสอบสิ่งกีดขวาง
-            
+            if (HasPlacement(x, y))
+            {
+                return true;
+            }
             return false;
         }
         void Move(Vector2 direction,GameObject targetMove)
